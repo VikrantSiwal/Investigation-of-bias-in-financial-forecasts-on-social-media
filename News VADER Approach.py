22a16b34f44c3586896db895af29edf337eab508
Vikrant Siwal <vikrantsiwal@gmail.com>
2019-08-14 16:50:56 +0100

    Update News VADER Approach.py

--- News VADER Approach.py
+++ News VADER Approach.py
@@ -2,7 +2,7 @@ import pandas as pd
 import re
 from vaderSentiment.vaderSentiment import SentimentIntensityAnalyzer
 analyser = SentimentIntensityAnalyzer()
-combined_news_data=pd.read_csv("E:/UCC Lecture Notes and e-books/Term 3/Project-Stock Market/Final Data/News/Combined file/All News.csv")
+combined_news_data=pd.read_csv("All News.csv")
 for index, row in combined_news_data.iterrows():
     text=row[3]
     
@@ -44,4 +44,4 @@ news['Overall']=news.sum(axis=1)
 news['Pos']=news['Positive']/news['Overall']
 news['Neu']=news['Neutral']/news['Overall']
 news['Neg']=news['Negative']/news['Overall']
-news[['Pos','Neu','Neg']].to_csv('E:/UCC Lecture Notes and e-books/Term 3/Project-Stock Market/Final Data/News/Combined file/processed_file.csv')
+news[['Pos','Neu','Neg']].to_csv('processed_file.csv')
