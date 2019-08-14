95c761c9c5f74bb8adf429392b106700673ce08f
Vikrant Siwal <vikrantsiwal@gmail.com>
2019-08-14 16:54:08 +0100

    Update Twitter Data Gathering Script.r

--- Twitter Data Gathering Script.r
+++ Twitter Data Gathering Script.r
@@ -9,11 +9,12 @@ library(topicmodels)
 library(ROAuth)
 library(ggplot2)
 
-consumer_key <- 'rZArj3q9ko4yYKMSejRsbLKL4'
-consumer_secret <- 'KtIICeKO47Os3F8wKgSoWpcAyxMjZBBWjijdnutGyHIxoGMnPG'
-access_token <- '705161064451682305-Hu2MEo79VIXHhm9pVsYZG9pcEKGNEW4'
-access_secret <- 'q3Um1i1oRk5FVfV4l1WKlR6Xb8ygHrcAAtboqKZth0IIy'
+consumer_key <- 'XXXXXXX'
+consumer_secret <- 'XXXXXXX'
+access_token <- 'XXXXXXXX'
+access_secret <- 'XXXXXXXX'
 setup_twitter_oauth(consumer_key, consumer_secret, access_token, access_secret)
 
 tweets_n <- searchTwitter("$Stock", n=10000,lang = "en")
 test_tweets <- twListToDF(tweets_n)
+write.csv(test_tweets, "Stock_name.csv")
